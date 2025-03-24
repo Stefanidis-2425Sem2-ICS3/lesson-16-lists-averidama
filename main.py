5d87389636c706904326d5593b31e2c92d0a42c2
averidama <damaa@students.dsbn.org>
2025-03-24 12:20:08 -0400

    finished

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#averid
+#lesson16
+#march24/25
+# working with lists
 
+# import random
+# num=[random.randint(0,100)for i in range(100)]
+# print(num)
+# count=100
+# average=int(sum(num)/count)
+# print ("the average of your numbers is",average)
+
+import random
+numberslist=[]
+for i in range(100):
+    num=random.randint(0,100)
+    numberslist.append(num)
+print(numberslist)
+count=100
+average=(sum(numberslist)/count)
+print ("the average of your numbers is",average)
